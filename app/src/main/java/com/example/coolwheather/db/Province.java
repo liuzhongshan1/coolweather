8632a0739d654de9237e7751fd78f5d5185c756f
liuzhongshan <liu.zhong.shan@foxmail.com>
2018-07-05 12:19:33 +0800

    database table set 创建配置数据库

---
+++ app/src/main/java/com/example/coolwheather/db/Province.java
@@ -0,0 +1,103 @@
+package com.example.coolwheather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 刘中山 on 2018-07-05.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private  String provinceName;
+    private int provinceCode;
+    public int getId(){
+        return id;
+    }
+
+    public String getProvinceName(){
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName){
+        this.provinceName = provinceName;
+
+    }
+
+    public int getProvinceCode(){
+        return provinceCode;
+    }
+
+
+    public void setProvinceCode(int provinceCode){
+        this.provinceCode = provinceCode;
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
